24747ab029fb43f8ae7280631152b1a74a05e427
Jen <evunti@gmail.com>
2025-07-01 16:45:21 -0400

    Refactor import statements and formatting in sanity.ts for consistency

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,8 +1,8 @@
-import { createClient } from '@sanity/client'
+import { createClient } from "@sanity/client";
 
 export const sanity = createClient({
-  projectId: 'a1xx4x23', // your project ID
-  dataset: 'production',
-  apiVersion: '2023-07-01', // use a recent date
+  projectId: "a1xx4x23", // your project ID
+  dataset: "production",
+  apiVersion: "2023-07-01", // use a recent date
   useCdn: true,
-})
+});
